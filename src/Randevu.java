e511c0c7d149b60a31af5866a5c4b1b9a1c4812b
iremdens <115092402+iremdens@users.noreply.github.com>
2022-12-30 16:45:00 +0300

    Add files via upload

---
+++ src/Randevu.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author irem
+ */
+public class Randevu extends HastaKayıt {
+
+    public String Servis;
+
+    public Randevu(int tc, String ad, String soyad, String dogumYeri, String dogumTarihi, String email, String servis) {
+        super(tc, ad, soyad, dogumYeri, dogumTarihi, email);
+        this.Servis = Servis;
+    }
+
+}
